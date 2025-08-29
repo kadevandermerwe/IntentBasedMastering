eb94315b1b305e926f8fda3d0a6ac05f6a0512ca
Kade van der Merwe <148642312+kadevandermerwe@users.noreply.github.com>
2025-08-29 15:51:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,6 @@ h2 { font-size: 16px !important; }
 # Top bar (plugin-like)
 st.markdown("""
     <div class='vale-nav'>
-        
         <div style='opacity:.65;'>Mastering Engineer</div>
         <div style='font-size:20px; font-weight:600;'>Vale</div>
         <div style='opacity:.6;'>You create ideas, we make them real.</div>
