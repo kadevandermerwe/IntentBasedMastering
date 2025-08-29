777506ee0976d39721bfe4932d2a526ff2ce99f6
Kade van der Merwe <148642312+kadevandermerwe@users.noreply.github.com>
2025-08-29 15:46:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ h2 { font-size: 16px !important; }
 st.markdown("""
     <div class='vale-nav'>
         <div style='display:flex; gap:10px; margin:0;'>
-            <div style='font-size:40px; font-weight:600;'>Vale</div>
+            <div style='font-size:20px; font-weight:600;'>Vale</div>
             <div style='opacity:.65;'>Mastering Engineer</div>
         </div>
         <div style='opacity:.6;'>You create ideas, we make them real.</div>
