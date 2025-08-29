da6d5ff11a079145cac37582e7e5023d53e4a597
Kade van der Merwe <148642312+kadevandermerwe@users.noreply.github.com>
2025-08-29 14:21:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ body {margin: auto;
 /* Avatar */
 .vale-header { display:flex; align-items:center; gap:10px; margin-bottom:8px; }
 
-.vale-nav {display: flex; position absolute; top: 0px;}
+.vale-nav {display: flex; position absolute; top: 0px; align-items: center;}
 
 .vale-avatar {
   position: relative;
