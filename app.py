dbcf5eceb30c3cc17f1581cdc38675fc0a4a910a
Kade van der Merwe <148642312+kadevandermerwe@users.noreply.github.com>
2025-08-29 16:32:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ st.markdown("""
 
 .vale-nav {
   display:flex;
-  justify-content:space-between;
+  justify-content:centre;
   text-align:centre;
   position: sticky;     /* stays at top on scroll */
   top: 0;
